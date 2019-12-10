71dac3af4b151ad6ad36afae98c9d9f50d1065ac
Adrien Thiery <adrien.thiery@osedea.com>
2019-12-10 13:58:30 -0500

    Renewed plugin for compatibility with react-native-firebase@v6

--- test/remove.js
+++ test/remove.js
@@ -2,16 +2,22 @@ const test = require('ava')
 const sinon = require('sinon')
 const plugin = require('../plugin')
 
-test('removes Firebase', async t => {
+test('removes react-native-firebase', async t => {
   const removeModule = sinon.spy()
-  const removePluginComponentExample = sinon.spy()
+  const ask = (question) => ({ [question.name]: [] })
+  const confirm = sinon.spy()
+  const info = sinon.spy()
+  const warning = sinon.spy()
+  const success = sinon.spy()
 
   const context = {
-    ignite: { removeModule, removePluginComponentExample }
+    ignite: { removeModule },
+    parameters: { options: {} },
+    prompt: { ask, confirm },
+    print: { info, warning, success },
   }
 
   await plugin.remove(context)
 
-  t.true(removeModule.calledWith('react-native-firebase', { unlink: true }))
-  t.true(removePluginComponentExample.calledWith('FirebaseExample.js'))
+  t.true(removeModule.calledWith('react-native-firebase', { unlink: false }))
 })
